3ada56eb0cb80fdff1c77758deca795bdd33f39b
Sunny <rajsaurya761@gmail.com>
2024-10-23 00:06:33 +0530

    working to finish

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,25 +8,25 @@ export default function About() {
           <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
             <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
               <div className="md:5/12 lg:w-5/12">
-                <img className="rounded-md"
+                <img
+                  className="rounded-md"
                   src="https://images.unsplash.com/photo-1483058712412-4245e9b90334?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="image"
                 />
               </div>
               <div className="md:7/12 lg:w-6/12">
                 <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                Our development is driven by the passion of dedicated developers
+                  Our development is driven by the passion of dedicated developers
                 </h2>
                 <p className="mt-6 text-gray-600">
                   Developer:
-                  <ul>
-                    <li>Name: Sunny Kumar Singh</li>
-                    <li>Scholar Number: 211112263</li>
-                  </ul>
                 </p>
-                <p className="mt-4 text-gray-600">
-                  Made using React+Vite+Firebase with pinch of Material UI and
-                  icond.
+                <div className=" text-gray-600">
+                  <div>Name: Sunny Kumar Singh</div>
+                  <div>Scholar Number: 211112263</div>
+                </div>
+                <p className="mt-2 text-gray-600">
+                  Made using React+Vite+Firebase with a pinch of Material UI and icond.
                 </p>
               </div>
             </div>
--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -5,47 +5,56 @@ import Layout from "../layout/Layout";
 const teamNames = [
   {
     id: 1,
-    image: "https://media.istockphoto.com/id/177427917/photo/close-up-of-red-cricket-ball-and-bat-sitting-on-grass.jpg?s=612x612&w=0&k=20&c=DcorerbBUeDNTfld3OclgHxCty4jih2yDCzipffX6zw=",
+    image:
+      "https://media.istockphoto.com/id/177427917/photo/close-up-of-red-cricket-ball-and-bat-sitting-on-grass.jpg?s=612x612&w=0&k=20&c=DcorerbBUeDNTfld3OclgHxCty4jih2yDCzipffX6zw=",
     title: "Cricket",
   },
   {
     id: 2,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM1seSPWBfiU4-pQoT8RbtWhu98eR1iQCUNQ&s",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM1seSPWBfiU4-pQoT8RbtWhu98eR1iQCUNQ&s",
     title: "Football",
   },
   {
     id: 3,
-    image: "https://media.istockphoto.com/id/183256716/photo/ball-and-basketball-court.jpg?s=612x612&w=0&k=20&c=dJwew7Yn4ltyv3pmKF-K9GVST_4LVMObSh-Wn5BzA3U=",
+    image:
+      "https://media.istockphoto.com/id/183256716/photo/ball-and-basketball-court.jpg?s=612x612&w=0&k=20&c=dJwew7Yn4ltyv3pmKF-K9GVST_4LVMObSh-Wn5BzA3U=",
     title: "Basketball",
   },
   {
     id: 4,
-    image: "https://media.istockphoto.com/id/1483011696/es/foto/pelota-de-tenis-raqueta-y-cancha-con-espacio-de-maqueta-fondo-borroso-o-sol-al-aire-libre.jpg?s=612x612&w=0&k=20&c=IDp_2jqpc6z1XhTQW6iiCCNbtUSQpT_kKp6ytmHW_PU=",
+    image:
+      "https://media.istockphoto.com/id/1483011696/es/foto/pelota-de-tenis-raqueta-y-cancha-con-espacio-de-maqueta-fondo-borroso-o-sol-al-aire-libre.jpg?s=612x612&w=0&k=20&c=IDp_2jqpc6z1XhTQW6iiCCNbtUSQpT_kKp6ytmHW_PU=",
     title: "Tennis",
   },
   {
     id: 5,
-    image: "https://media.istockphoto.com/id/1473259144/photo/volleyball-ball-and-net-in-voleyball-arena-during-a-match.jpg?s=612x612&w=0&k=20&c=vbn1S1xDKY7XO5lGdR14FCLhHkWXcjoS49j4-Cc1Ukg=",
+    image:
+      "https://media.istockphoto.com/id/1473259144/photo/volleyball-ball-and-net-in-voleyball-arena-during-a-match.jpg?s=612x612&w=0&k=20&c=vbn1S1xDKY7XO5lGdR14FCLhHkWXcjoS49j4-Cc1Ukg=",
     title: "Volleyball",
   },
   {
     id: 6,
-    image: "https://media.istockphoto.com/id/1440967752/photo/field-hockey-player-on-artificial-grass-play-field.jpg?s=612x612&w=0&k=20&c=xj3wRZt5eYEppMPIuGqDHdPGbrN0HqZikuqyOVQwyNo=",
+    image:
+      "https://media.istockphoto.com/id/1440967752/photo/field-hockey-player-on-artificial-grass-play-field.jpg?s=612x612&w=0&k=20&c=xj3wRZt5eYEppMPIuGqDHdPGbrN0HqZikuqyOVQwyNo=",
     title: "Hockey",
   },
   {
     id: 7,
-    image: "https://images.unsplash.com/photo-1609710228159-0fa9bd7c0827?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGFibGUlMjB0ZW5uaXN8ZW58MHx8MHx8fDA%3D",
+    image:
+      "https://images.unsplash.com/photo-1609710228159-0fa9bd7c0827?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGFibGUlMjB0ZW5uaXN8ZW58MHx8MHx8fDA%3D",
     title: "TableTennis",
   },
   {
     id: 9,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqArlAxGNo7myzYEjatDOttui5ny6SwSx37w&s",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqArlAxGNo7myzYEjatDOttui5ny6SwSx37w&s",
     title: "Badminton",
   },
   {
     id: 10,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNR4Bo8x9HMMkq3M3kr0nJd6udEhG0TwKkcQ&s",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNR4Bo8x9HMMkq3M3kr0nJd6udEhG0TwKkcQ&s",
     title: "Chess",
   },
 ];
@@ -70,10 +79,12 @@ const Teams = () => {
                 const { image, title } = item;
                 return (
                   <div key={index} className="p-4 w-full md:w-1/4">
-                    <div className="h-full border border-gray-300 rounded-3xl overflow-hidden shadow-md cursor-pointer">
+                    <div
+                      className="h-full border border-gray-300 rounded-3xl overflow-hidden shadow-md cursor-pointer"
+                      onClick={() => navigate(`/teams/${title}`)}
+                    >
                       <img
-                        onClick={() => navigate(`/teams/${title}`)} // Navigate to the selected team
-                        className="lg:h-80  h-96 w-full"
+                        className="lg:h-60  h-96 w-full"
                         src={image}
                         alt="team"
                       />
